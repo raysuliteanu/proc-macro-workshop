f15b9ad57200d98895e37b2383737062849e3844
Ray Suliteanu <raysuliteanu@gmail.com>
2024-11-21 22:11:25 -0800

    make generic

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,89 +1,133 @@
-use proc_macro::TokenStream;
 use proc_macro2::{Ident, Span};
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, Data, DeriveInput, Field};
 
 #[proc_macro_derive(Builder)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as DeriveInput);
-    let ident = input.ident;
-    let builder_struct = Ident::new(&format!("{}Builder", ident), Span::call_site());
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let derived_input = parse_macro_input!(input as DeriveInput);
 
-    quote! {
-        impl #ident {
-            pub fn builder() -> #builder_struct {
-                #builder_struct {
-                    executable: None,
-                    args: None,
-                    env: None,
-                    current_dir: None,
+    let target_struct_name = derived_input.ident;
+    let builder_struct_name =
+        Ident::new(&format!("{}Builder", target_struct_name), Span::call_site());
+
+    let fields = &builder_fields(&derived_input.data);
+
+    let init_builder_fields = fields.iter().map(|field| {
+        let ident = field.ident.as_ref().unwrap();
+        quote! {
+            #ident: None
+        }
+    });
+
+    let builder_function = quote! {
+        impl #target_struct_name {
+            pub fn builder() -> #builder_struct_name {
+                #builder_struct_name {
+                    #(#init_builder_fields,)*
                 }
-
             }
         }
+    };
 
-        struct #builder_struct {
-            executable: Option<String>,
-            args: Option<Vec<String>>,
-            env: Option<Vec<String>>,
-            current_dir: Option<String>,
+    let builder_fields = fields.iter().map(|field| {
+        let ident = field.ident.as_ref().unwrap();
+        let ty = &field.ty;
+        quote! {
+            #ident: ::std::option::Option<#ty>
         }
+    });
 
-        impl #builder_struct {
-            pub fn build(&mut self) -> Result<#ident, Box<dyn std::error::Error>> {
-                let executable = if let Some(e) = self.executable.take() {
-                    e
-                } else {
-                     return Err("missing executable".into());
-                };
+    let builder_struct = quote! {
+        struct #builder_struct_name {
+            #(#builder_fields,)*
+        }
+    };
 
-                let args = if let Some(a) = self.args.take() {
-                    a
-                } else {
-                     return Err("missing args".into());
-                };
+    let build_fn_setters = fields.iter().map(|field| {
+        let ident = field.ident.as_ref().unwrap();
+        quote! {
+            let #ident = self.#ident.take().unwrap();
+        }
+    });
 
-                let env = if let Some(e) = self.env.take() {
-                    e
-                } else {
-                     return Err("missing env".into());
-                };
+    let build_fn_fields = fields.iter().map(|field| {
+        let ident = field.ident.as_ref().unwrap();
+        quote! {
+            #ident
+        }
+    });
 
-                let current_dir = if let Some(c) = self.current_dir.take() {
-                    c
-                } else {
-                     return Err("missing current_dir".into());
-                };
+    let build_fn = quote! {
+        pub fn build(&mut self) -> ::core::result::Result<#target_struct_name, Box<dyn std::error::Error>> {
+            #(#build_fn_setters)*
 
-                Ok(#ident {
-                    executable,
-                    args,
-                    env,
-                    current_dir,
-                })
-            }
+            Ok(#target_struct_name {
+                #(#build_fn_fields,)*
+            })
+        }
+    };
 
-            fn executable(&mut self, executable: String) -> &mut Self {
-                self.executable = Some(executable);
+    let builder_fns = fields.iter().map(|field| {
+        let ident = field.ident.as_ref().unwrap();
+        let ty = &field.ty;
+
+        quote! {
+            fn #ident(&mut self, #ident: #ty) -> &mut Self {
+                self.#ident = Some(#ident);
                 self
             }
+        }
+    });
 
-            fn args(&mut self, args: Vec<String>) -> &mut Self {
-                self.args = Some(args);
-                self
+    let ts = quote! {
+        #builder_function
+
+        #builder_struct
+
+        impl #builder_struct_name {
+            #build_fn
+
+            #(#builder_fns)*
+        }
+    };
+
+    eprintln!("TOKENS: {:#?}", ts);
+
+    proc_macro::TokenStream::from(ts)
+}
+
+fn builder_fields(data: &Data) -> Vec<&Field> {
+    match *data {
+        Data::Struct(ref ds) => match ds.fields {
+            syn::Fields::Named(ref f) => f.named.iter().collect(),
+            syn::Fields::Unnamed(_) | syn::Fields::Unit => {
+                unimplemented!("#[derive(Builder)] can only be used on structs")
             }
-
-            fn env(&mut self, env: Vec<String>) -> &mut Self {
-                self.env = Some(env);
-                self
-            }
-
-
-            fn current_dir(&mut self, current_dir: String) -> &mut Self {
-                self.current_dir = Some(current_dir);
-                self
-            }
+        },
+        Data::Enum(_) | Data::Union(_) => {
+            unimplemented!("#[derive(Builder)] can only be used on structs")
         }
     }
-    .into()
+}
+
+fn is_optional_field(data: Data, name: &str) -> bool {
+    match data {
+        Data::Struct(ds) => {
+            let fields = ds.fields;
+            match fields {
+                syn::Fields::Named(f) => {
+                    for field in f.named {
+                        let fname = quote! {#field.ident}.to_string();
+                        let ty = quote! {#field.ty}.to_string();
+                        if fname == name && ty.starts_with("Option") {
+                            return true;
+                        }
+                    }
+                    false
+                }
+                syn::Fields::Unnamed(_) | syn::Fields::Unit => false,
+            }
+        }
+        Data::Enum(_) | Data::Union(_) => false,
+    }
 }
